d0af166a6380a5e39d25170f3cf707b080b8d883
Gunkut Zeybek <gunkut.zeybek@gmail.com>
2021-07-12 15:08:09 +0300

    Updated terraform file and github actions yaml

--- main.tf
+++ main.tf
@@ -3,9 +3,35 @@ provider "azurerm" {
     features {}
 }
 
+terraform{
+    backend "remote" {
+        organization: "gunkut_dev"
+
+        workspaces {
+            name = "ItemTrader-APIDriven"
+        }
+    }
+}
+
 resource "azurerm_resource_group" "gunkut_dev" {
     name = "itemtrader-tf"
     location = "westus2"
+}
+
+resource "azurerm_sql_sqrver" "gunkut_dev" {
+    name = var.SQL_SERVER_NAME
+    resource_group_name = azurerm_resource_group.gunkut_dev.name
+    location = azurerm_resource_group.gunkut_dev.location
+    version = "12.0"
+    administrator_login = var.SQL_DB_USER
+    asministrator_login_password = var.SQL_DB_PASS
+}
+
+resource "azurerm_sql_database" "gunkut_dev" {
+    name = ItemTraderDB
+    resource_group_name = azurerm_resource_group.gunkut_dev.name
+    location = azurerm_resource_group.gunkut_dev.location
+    server_name = azurerm_sql_sqrver.gunkut_dev.name
 }
 
 resource "azurerm_app_service_plan" "gunkut_dev" {
@@ -15,16 +41,36 @@ resource "azurerm_app_service_plan" "gunkut_dev" {
 
     sku {
         tier = "Free"
-        size = "S0"
-    }
+        size = "F0"
+    }    
 }
 
 resource "azurerm_app_service" "gunkut_dev" {
     name = "itemtrader-appservice"
     location = azurerm_resource_group.gunkut_dev.location
     resource_group_name = azurerm_resource_group.gunkut_dev.name
-    app_service_plan_id = azurerm_app_service_plan.gunkut_dev.id    
+    app_service_plan_id = azurerm_app_service_plan.gunkut_dev.id   
+
+    connection_string {
+        name = "DefaultConnection"
+        type = "sqlserver"
+        value = "Server=azurerm_sql_server.gunkut_dev.fully_qualified_domain_name;Database=azurerm_sql_database.gunkut_dev.name;uid=var.SQL_DB_USER;var.SQL_DB_PASS"
+    }
 }
+
+resource "azurerm_app_service" "gunkut_dev" {
+    name = "itemtrader-authserver"
+    location = azurerm_resource_group.gunkut_dev.location
+    resource_group_name = azurerm_resource_group.gunkut_dev.name
+    app_service_plan_id = azurerm_app_service_plan.gunkut_dev.id   
+
+    connection_string {
+        name = "DefaultConnection"
+        type = "sqlserver"
+        value = "Server=azurerm_sql_server.gunkut_dev.fully_qualified_domain_name;Database=azurerm_sql_database.gunkut_dev.name;uid=var.SQL_DB_USER;var.SQL_DB_PASS"
+    }
+}
+
 
 
 
