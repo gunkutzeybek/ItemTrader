f38a9552529615091b6d50524bc30ca14fd1c8cd
Gunkut Zeybek <gunkut.zeybek@gmail.com>
2021-07-12 23:19:28 +0300

    requireHttpsMetada is set to true agian

--- src/ItemTrader.WebAPI/Startup.cs
+++ src/ItemTrader.WebAPI/Startup.cs
@@ -43,7 +43,6 @@ namespace ItemTrader.Api
                     {
                         ValidateAudience = false
                     };
-                    options.RequireHttpsMetadata = false;
                 });
         }
 
