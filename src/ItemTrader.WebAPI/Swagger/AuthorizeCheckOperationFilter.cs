a417bb73de5cc0bab9151fca28a33e055ba483e4
Gunkut Zeybek <gunkut.zeybek@gmail.com>
2021-07-13 10:51:21 +0300

    adding status codes to swagger fixed

--- src/ItemTrader.WebAPI/Swagger/AuthorizeCheckOperationFilter.cs
+++ src/ItemTrader.WebAPI/Swagger/AuthorizeCheckOperationFilter.cs
@@ -16,11 +16,16 @@ namespace ItemTrader.Api.Swagger
 
             if (hasAuthorize)
             {
-                operation.Responses.Add("200", new OpenApiResponse{Description = "OK"});
-                operation.Responses.Add("201", new OpenApiResponse { Description = "Created" });
-                operation.Responses.Add("204", new OpenApiResponse { Description = "NoContent" });
-                operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
-                operation.Responses.Add("404", new OpenApiResponse { Description = "NotFound" });
+                if (!operation.Responses.ContainsKey("200"))
+                    operation.Responses.Add("200", new OpenApiResponse{Description = "OK"});
+                if (!operation.Responses.ContainsKey("201"))
+                    operation.Responses.Add("201", new OpenApiResponse { Description = "Created" });
+                if (!operation.Responses.ContainsKey("204"))
+                    operation.Responses.Add("204", new OpenApiResponse { Description = "NoContent" });
+                if (!operation.Responses.ContainsKey("401"))
+                    operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
+                if (!operation.Responses.ContainsKey("404"))
+                    operation.Responses.Add("404", new OpenApiResponse { Description = "NotFound" });
 
 
                 operation.Security = new List<OpenApiSecurityRequirement>
